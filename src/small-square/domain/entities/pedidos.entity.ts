60ff714e3c5ad36d626c9e4eef7947f0d70d324d
Erik Stor <erik.stor.franco@gmail.com>
2023-06-02 14:50:11 -0500

    cambio de base de datos

--- src/small-square/domain/entities/pedidos.entity.ts
+++ src/small-square/domain/entities/pedidos.entity.ts
@@ -1,63 +1,60 @@
 import {
-    Column,
-    Entity,
-    JoinColumn,
-    ManyToOne, OneToMany,
-    PrimaryColumn,
-    PrimaryGeneratedColumn
-} from "typeorm"
-import {RestaurantesEntity} from "./restaurantes.entity";
-import {RestauranteEmpleadoEntity} from "./restaurante-empleado.entity";
-import {UsuariosEntity} from "./usuarios.entity";
-import {estados} from "../../app/interfaces/order.interfaces";
-import {PedidosPlatosEntity} from "./pedidos-platos.entity";
-import { randomInt } from "crypto";
-
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { RestaurantesEntity } from './restaurantes.entity';
+import { RestauranteEmpleadoEntity } from './restaurante-empleado.entity';
+import { UsuariosEntity } from './usuarios.entity';
+import { estados } from '../../app/interfaces/order.interfaces';
+import { PedidosPlatosEntity } from './pedidos-platos.entity';
+import { randomInt } from 'crypto';
 
 @Entity({
-    name: 'pedidos'
+  name: 'pedidos',
 })
 export class PedidosEntity {
+  @PrimaryGeneratedColumn('increment')
+  @PrimaryColumn('bigint')
+  id?: number;
 
-    @PrimaryGeneratedColumn('increment')
-    @PrimaryColumn('bigint')
-    id?: number
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  fecha!: Date;
 
-    @Column({type: 'date', default: new Date()})
-    fecha!: Date
+  @Column({
+    type: 'enum',
+    enum: estados,
+    default: estados.PEN,
+  })
+  estado!: estados;
 
+  @Column('varchar')
+  descripcion!: string;
 
-    @Column({type: 'enum', enum: estados, default: estados.PEN})
-    estado!: estados
+  @Column('int', { default: randomInt(9999) })
+  codigo!: number;
 
-    @Column('varchar')
-    descripcion!: string
+  @Column()
+  @ManyToOne(() => UsuariosEntity, (user) => user.id, {
+    eager: true,
+  })
+  @JoinColumn({ name: 'id_cliente' })
+  id_cliente: UsuariosEntity | number;
 
-    @Column('int', {default: randomInt(9999)})
-    codigo!: number
+  @Column()
+  @ManyToOne(() => RestaurantesEntity, (restaurant) => restaurant.id)
+  @JoinColumn({ name: 'id_restaurante' })
+  id_restaurante!: RestaurantesEntity | number;
 
-    @Column()
-    @ManyToOne(
-        () => UsuariosEntity,
-        (user) => user.id,
-        {
-            eager: true
-        }
-    )
-    @JoinColumn({name: 'id_cliente'})
-    id_cliente: UsuariosEntity | number
+  @Column({ nullable: true })
+  @ManyToOne(() => RestauranteEmpleadoEntity)
+  @JoinColumn({ name: 'id_chef' })
+  id_chef?: RestauranteEmpleadoEntity | number;
 
-    @Column()
-    @ManyToOne(() => RestaurantesEntity, restaurant => restaurant.id)
-    @JoinColumn({name: 'id_restaurante'})
-    id_restaurante!: RestaurantesEntity | number
-
-    @Column({nullable: true})
-    @ManyToOne(() => RestauranteEmpleadoEntity)
-    @JoinColumn({name: 'id_chef' })
-    id_chef?: RestauranteEmpleadoEntity | number
-
-    @OneToMany(() => PedidosPlatosEntity, pp => pp.pedido)
-    pedidos_platos!: PedidosPlatosEntity | undefined
-
+  @OneToMany(() => PedidosPlatosEntity, (pp) => pp.pedido)
+  pedidos_platos!: PedidosPlatosEntity | undefined;
 }
